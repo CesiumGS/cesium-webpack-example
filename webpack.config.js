dd4e010d8f5cd83e29cfc46a69f3cfdccb0e0bb2
ggetz <ggetz881@gmail.com>
2017-10-05 16:22:15 -0400

    file-loader to url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,17 +16,23 @@ module.exports = [{
 	output: {
 		filename: '[name].js',
 		path: path.resolve(__dirname, 'dist'),
-		sourcePrefix: '' // Needed by Cesium for multiline strings
+
+		// Needed by Cesium for multiline strings
+		sourcePrefix: '' 
 	},
+	amd: {
+		// Enable webpack-friendly use of require in cesium
+		toUrlUndefined: true
+	},
+	node: {
+		// Resolve node module use of fs
+		fs: "empty"
+    },
 	resolve: {
 		alias: {
 			// Cesium module name
 			cesium: path.resolve(__dirname, cesiumSource)
 		}
-	},
-	amd: {
-		// Enable webpack-friendly use of require in cesium
-		toUrlUndefined: true
 	},
 	module: {
 		rules: [{
@@ -34,30 +40,25 @@ module.exports = [{
 			use: [ 'style-loader', 'css-loader' ]
 		}, {
 			test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
-            use: [ 'file-loader' ]
+            use: [ 'url-loader' ]
 		}]
 	},
 	plugins: [
 	    new HtmlWebpackPlugin({
 	        template: 'src/index.html'
     	}),
-    	new webpack.DefinePlugin({
-    		// Define relative base path in cesium for loading assets
-  			CESIUM_BASE_URL: JSON.stringify('')
-		}),
 		// Copy Cesium Assets, Widgets, and Workers to a static directory
     	new CopyWebpackPlugin([ { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' } ]),
     	new CopyWebpackPlugin([ { from: path.join(cesiumSource, 'Assets'), to: 'Assets' } ]),
-    	new CopyWebpackPlugin([ { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' } ])
-    ],
-	node: {
-		// Resolve node module use of fs
-		fs: "empty"
-    },
+    	new CopyWebpackPlugin([ { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' } ]),
+    	new webpack.DefinePlugin({
+    		// Define relative base path in cesium for loading assets
+  			CESIUM_BASE_URL: JSON.stringify('')
+		})
+    ]
 
 	// development server options
 	devServer: {
 		contentBase: './dist'
-	},
-	devtool: 'eval'
+	}
 }];
