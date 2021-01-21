58be8452be4cfbaecb02a17de4a188c3e097e763
Michael Holthausen <mholthausen@terrestris.de>
2021-01-21 23:35:06 +0100

    Updates dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 import { Cesium3DTileset, createWorldTerrain, IonResource, Viewer } from 'cesium';
-import "cesium/Build/Cesium/Widgets/widgets.css";
+import "../node_modules/cesium/Build/Cesium/Widgets/widgets.css";
 import "./css/main.css";
 
 // This is simplified version of Cesium's Getting Started tutorial.
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,71 @@
-const path = require('path');
-
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
 
-module.exports = [{
-    mode: 'development',
-    context: __dirname,
-    entry: {
-        app: './src/index.js'
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devtool: 'eval',
-    node: {
-        // Resolve node module use of fs
-        fs: "empty",
-        Buffer: false,
-        http: "empty",
-        https: "empty",
-        zlib: "empty"
-    },
-    resolve: {
-        mainFields: ['module', 'main']
-    },
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-        }, {
-            test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
-            use: ['url-loader']
-        }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        }),
-        // Copy Cesium Assets, Widgets, and Workers to a static directory
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'node_modules/cesium/Build/Cesium/Workers', to: 'Workers' },
-                { from: 'node_modules/cesium/Build/Cesium/ThirdParty', to: 'ThirdParty' },
-                { from: 'node_modules/cesium/Build/Cesium/Assets', to: 'Assets' },
-                { from: 'node_modules/cesium/Build/Cesium/Widgets', to: 'Widgets' }
-            ],
-        }),
-        new webpack.DefinePlugin({
-            // Define relative base path in cesium for loading assets
-            CESIUM_BASE_URL: JSON.stringify('')
-        })
-    ],
+module.exports = {
+  mode: 'development',
+  context: __dirname,
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval',
+  node: {
+    // Resolve node module use of fs
+    global: false,
+    __filename: false,
+    __dirname: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    })
+  ],
+  resolve: {
+    mainFields: ['module', 'main']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+        use: ['url-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    // Copy Cesium Assets, Widgets, and Workers to a static directory
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'node_modules/cesium/Build/Cesium/Workers', to: 'Workers' },
+        {
+          from: 'node_modules/cesium/Build/Cesium/ThirdParty',
+          to: 'ThirdParty'
+        },
+        { from: 'node_modules/cesium/Build/Cesium/Assets', to: 'Assets' },
+        { from: 'node_modules/cesium/Build/Cesium/Widgets', to: 'Widgets' }
+      ]
+    }),
+    new webpack.DefinePlugin({
+      // Define relative base path in cesium for loading assets
+      CESIUM_BASE_URL: JSON.stringify('')
+    }),
+    new TerserPlugin()
+  ],
 
-    // development server options
-    devServer: {
-        contentBase: path.join(__dirname, "dist")
-    }
-}];
+  // development server options
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  }
+};
