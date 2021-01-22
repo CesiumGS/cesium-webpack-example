0b5b1a01c5cbd622a1481d4353a30eda1a350310
Michael Holthausen <mholthausen@terrestris.de>
2021-01-22 12:11:22 +0100

    Updates webconfig release node

--- webpack.release.config.js
+++ webpack.release.config.js
@@ -4,70 +4,79 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = [{
+module.exports = [
+  {
     mode: 'production',
     context: __dirname,
     entry: {
-        app: './src/index.js'
+      app: './src/index.js'
     },
     output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist')
     },
     node: {
-        // Resolve node module use of fs
-        fs: "empty",
-        Buffer: false,
-        http: "empty",
-        https: "empty",
-        zlib: "empty"
+      // Resolve node module use of fs
+      global: false,
+      __filename: false,
+      __dirname: false
     },
     resolve: {
-        mainFields: ['module', 'main']
+      mainFields: ['module', 'main']
     },
     module: {
-        rules: [{
-            test: /\.css$/,
-            use: ['style-loader', { loader: 'css-loader' }],
-            sideEffects: true
-        }, {
-            test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
-            use: ['url-loader']
-        }, {
-            // Remove pragmas
-            test: /\.js$/,
-            enforce: 'pre',
-            include: path.resolve(__dirname, 'node_modules/cesium/Source'),
-            sideEffects: false,
-            use: [{
-                loader: 'strip-pragma-loader',
-                options: {
-                    pragmas: {
-                        debug: false
-                    }
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', { loader: 'css-loader' }],
+          sideEffects: true
+        },
+        {
+          test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+          use: ['url-loader']
+        },
+        {
+          // Remove pragmas
+          test: /\.js$/,
+          enforce: 'pre',
+          include: path.resolve(__dirname, 'node_modules/cesium/Source'),
+          sideEffects: false,
+          use: [
+            {
+              loader: 'strip-pragma-loader',
+              options: {
+                pragmas: {
+                  debug: false
                 }
-            }]
-        }]
+              }
+            }
+          ]
+        }
+      ]
     },
     optimization: {
-        usedExports: true
+      usedExports: true
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        }),
-        // Copy Cesium Assets, Widgets, and Workers to a static directory
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'node_modules/cesium/Build/Cesium/Workers', to: 'Workers' },
-                { from: 'node_modules/cesium/Build/Cesium/ThirdParty', to: 'ThirdParty' },
-                { from: 'node_modules/cesium/Build/Cesium/Assets', to: 'Assets' },
-                { from: 'node_modules/cesium/Build/Cesium/Widgets', to: 'Widgets' }
-            ],
-        }),
-        new webpack.DefinePlugin({
-            // Define relative base path in cesium for loading assets
-            CESIUM_BASE_URL: JSON.stringify('')
-        })
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      }),
+      // Copy Cesium Assets, Widgets, and Workers to a static directory
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'node_modules/cesium/Build/Cesium/Workers', to: 'Workers' },
+          {
+            from: 'node_modules/cesium/Build/Cesium/ThirdParty',
+            to: 'ThirdParty'
+          },
+          { from: 'node_modules/cesium/Build/Cesium/Assets', to: 'Assets' },
+          { from: 'node_modules/cesium/Build/Cesium/Widgets', to: 'Widgets' }
+        ]
+      }),
+      new webpack.DefinePlugin({
+        // Define relative base path in cesium for loading assets
+        CESIUM_BASE_URL: JSON.stringify('')
+      })
     ]
-}];
+  }
+];
