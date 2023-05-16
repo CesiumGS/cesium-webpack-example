2d69b1f6264fe20238be61c8739af4e32a0fb820
Katrin Schmid <3a1b2c3@protonmail.com>
2023-05-17 08:05:06 +1000

    working

--- src/index.js
+++ src/index.js
@@ -1,18 +1,45 @@
-import { Ion, Viewer, createWorldTerrain, createOsmBuildings, Cartesian3, Math } from "cesium";
+import { Ion, Viewer, Cesium3DTileset, createGooglePhotorealistic3DTileset, GoogleMaps, createWorldTerrain, createOsmBuildings, Cartesian3, Math } from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import "../src/css/main.css"
 
 // Your access token can be found at: https://cesium.com/ion/tokens.
 // This is the default access token
-Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYWE1OWUxNy1mMWZiLTQzYjYtYTQ0OS1kMWFjYmFkNjc5YzciLCJpZCI6NTc3MzMsImlhdCI6MTYyNzg0NTE4Mn0.XcKpgANiY19MC4bdFUXMVEBToBmqS8kuYpUlxJHYZxk';
+Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0ODRkYWU2My1iZjBmLTRkMjktYmY1NS03YWUzY2UxNzNhMDEiLCJpZCI6MjU1NDYsImlhdCI6MTY4NDAxNTc3N30.Tk7hqVGbeT0yUKC2qMIgvFRUsRlQO_f4zkMpPg3s0n8';
+GoogleMaps.defaultApiKey = "AIzaSyAZ8EhV6n0W2ju3uBvfR8AGn6kWuawwoyE";
 
-// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-const viewer = new Viewer('cesiumContainer', {
-  terrainProvider: createWorldTerrain()
+const viewer = new Viewer("cesiumContainer", {
+  timeline: false,
+  animation: false,
+  sceneModePicker: false,
+  baseLayerPicker: false,
 });
 
-// Add Cesium OSM Buildings, a global 3D buildings layer.
-viewer.scene.primitives.add(createOsmBuildings());   
+viewer.scene.globe.show = false;
+
+/*
+ const tileset = await createGooglePhotorealistic3DTileset();
+ createGooglePhotorealistic3DTileset().then()
+ tileset.debugShowViewerRequestVolume = true;
+       viewer.scene.primitives.add(tileset);
+          console.log("tileset: ", tileset._url)
+          
+
+              getCounter(true).then((c) => {
+      if (c > 0) {
+        setCounter(c);
+      }
+    });
+*/
+  createGooglePhotorealistic3DTileset().then((t) => {
+    viewer.scene.primitives.add(t);
+    t.debugShowBoundingVolume = true;
+  });
+ //viewer.scene.primitives.add(createGooglePhotorealistic3DTileset());
+
+  const tileset = viewer.scene.primitives.add(new Cesium3DTileset({
+      url: "https://tile.googleapis.com/v1/3dtiles/root.json?key=AIzaSyAZ8EhV6n0W2ju3uBvfR8AGn6kWuawwoyE",
+      showCreditsOnScreen: true,
+    }));
 
 // Fly the camera to San Francisco at the given longitude, latitude, and height.
 viewer.camera.flyTo({
