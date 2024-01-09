afc288cea978d23795a7fa81a88d60419fbcfe44
Josh <8007967+jjspace@users.noreply.github.com>
2024-01-09 12:10:18 -0500

    Add webpack 4 example

---
+++ webpack-4/src/index.js
@@ -0,0 +1,26 @@
+import { Viewer, Cartesian3, Math, Terrain, createOsmBuildingsAsync } from "cesium";
+import "cesium/Build/Cesium/Widgets/widgets.css";
+import "./css/main.css";
+
+// CesiumJS has a default access token built in but it's not meant for active use.
+// please set your own access token can be found at: https://cesium.com/ion/tokens.
+// Ion.defaultAccessToken = "YOUR TOKEN HERE";
+
+// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+const viewer = new Viewer("cesiumContainer", {
+  terrain: Terrain.fromWorldTerrain(),
+});
+
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+createOsmBuildingsAsync().then((buildingTileset) => {
+  viewer.scene.primitives.add(buildingTileset);
+});
+
+// Fly the camera to San Francisco at the given longitude, latitude, and height.
+viewer.camera.flyTo({
+  destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+  orientation: {
+    heading: Math.toRadians(0.0),
+    pitch: Math.toRadians(-15.0),
+  },
+});
---
+++ webpack-4/webpack.config.js
@@ -0,0 +1,92 @@
+"use strict";
+
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const cesiumSource = "node_modules/cesium/Build/Cesium";
+// this is the base url for static files that CesiumJS needs to load
+// Not required but if it's set remember to update CESIUM_BASE_URL as shown below
+const cesiumBaseUrl = "cesiumStatic";
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  // resolve: {
+  //   fallback: { https: false, zlib: false, http: false, url: false },
+  //   mainFiles: ["index", "Cesium"],
+  // },
+  module: {
+    unknownContextCritical: false,
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: {
+          and: [/node_modules/], // Exclude libraries in node_modules ...
+          not: [/cesium/], // Except Cesium because it uses modern syntax
+        },
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [["@babel/preset-env", { targets: "defaults, not ie 11" }]],
+              plugins: ["@babel/plugin-transform-optional-chaining"],
+            },
+          },
+          // Babel understands import.meta but Webpack doesn't
+          // can't find a way to tell babel not to output it so we need another loader
+          // that can understand it and translate into Webpack's representation
+          // https://www.npmjs.com/package/@open-wc/webpack-import-meta-loader
+          // TODO: but then we get static dependency errors about `require` in buildModuleUrl instead
+          require.resolve("@open-wc/webpack-import-meta-loader"),
+        ],
+      },
+      {
+        test: /\.css/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    // Copy Cesium Assets, Widgets, and Workers to a static directory
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.join(cesiumSource, "Workers"),
+          to: `${cesiumBaseUrl}/Workers`,
+        },
+        {
+          from: path.join(cesiumSource, "ThirdParty"),
+          to: `${cesiumBaseUrl}/ThirdParty`,
+        },
+        {
+          from: path.join(cesiumSource, "Assets"),
+          to: `${cesiumBaseUrl}/Assets`,
+        },
+        {
+          from: path.join(cesiumSource, "Widgets"),
+          to: `${cesiumBaseUrl}/Widgets`,
+        },
+      ],
+    }),
+    new webpack.DefinePlugin({
+      // Define relative base path in cesium for loading assets
+      CESIUM_BASE_URL: JSON.stringify(cesiumBaseUrl),
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+  },
+  mode: "development",
+};
